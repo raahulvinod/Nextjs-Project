281f547be8cfd953e2eb1cc040e50b97ac70ee57
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-05-27 19:12:07 +0530

    Post Submission Added

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -10,15 +10,12 @@ const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  });
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: '', tag: '' });
 
   const createPrompt = async (e) => {
     e.preventDefault();
-    setSubmitting(true);
+    setIsSubmitting(true);
 
     try {
       const response = await fetch('/api/prompt/new', {
@@ -36,7 +33,7 @@ const CreatePrompt = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setSubmitting(false);
+      setIsSubmitting(false);
     }
   };
 
