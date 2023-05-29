b8c8cb604486da301a3842a44a27f3658f9c7c11
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-05-29 16:08:21 +0530

    Delete Prompt Added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -15,7 +15,27 @@ const MyProfile = () => {
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      'Are you sure you want to delete this prompt?'
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter((p) => {
+          _id !== post.id;
+        });
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchPost = async () => {
--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -27,29 +27,30 @@ const EditPrompt = () => {
     if (promptId) getPromptDetails();
   }, [promptId]);
 
-  //   const createPrompt = async (e) => {
-  //     e.preventDefault();
-  //     setIsSubmitting(true);
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
 
-  //     try {
-  //       const response = await fetch('/api/prompt/new', {
-  //         method: 'POST',
-  //         body: JSON.stringify({
-  //           prompt: post.prompt,
-  //           userId: session?.user.id,
-  //           tag: post.tag,
-  //         }),
-  //       });
+    if (!promptId) return alert('Prompt ID not found');
 
-  //       if (response.ok) {
-  //         router.push('/');
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setIsSubmitting(false);
-  //     }
-  //   };
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <Form
@@ -57,7 +58,7 @@ const EditPrompt = () => {
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={() => {}}
+      handleSubmit={updatePrompt}
     />
   );
 };
