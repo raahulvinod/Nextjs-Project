cd7afb1ccaa3a9355ec28428f3d9eeac5ec463ea
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-05-30 12:58:28 +0530

    Final changes

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
-        Promtopia is an open-source AI promting tool for modern world to
+        Promptopia is an open-source AI promting tool for modern world to
         discover, create and share creative prompts
       </p>
 
